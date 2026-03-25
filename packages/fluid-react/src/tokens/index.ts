c4d38095b6eaff4cf9ede498f76f7ff12d8d9ed5
Amarnath B R <digitallyamar@gmail.com>
2026-03-25 07:29:07 +0000

    feat: add token contract and validation

--- packages/fluid-react/src/tokens/index.ts
+++ packages/fluid-react/src/tokens/index.ts
@@ -1,2 +1,4 @@
 export { createTheme } from "./createTheme";
 export type { ThemeContract } from "./contract";
+export { constructionPreset } from "./presets/construction";
+export { medicinePreset } from "./presets/medicine";
